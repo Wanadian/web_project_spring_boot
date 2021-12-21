b62b969c201cc34a20f19ac1f2540c0a4f2d0ed3
Wonkru <williamdpe@gmail.com>
2021-12-21 20:51:17 +0100

    creating welcome page

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Welcome from "./components/Welcome";
 import SongList from "./components/SongList";
 
 function App() {
-  return (
-      <div className={"App"}>
-        <SongList>
-            
-        </SongList>
-      </div>
-  )
-  ;
+    return (
+        <div className={'App'}>
+            <Router>
+                <Routes>
+                    <Route exact path={''} element={<Welcome/>}/>
+                    <Route exact path={'/songs'} element={<SongList/>}/>
+                </Routes>
+            </Router>
+        </div>
+    )
+        ;
 }
 
 export default App;
---
+++ app/src/components/Welcome.js
@@ -0,0 +1,16 @@
+import './Welcome.css'
+
+function Welcome(){
+    return(
+        <div className={'Welcome'}>
+            <div>
+                Welcome to
+            </div>
+            <div>
+                Echo
+            </div>
+        </div>
+    );
+}
+
+export default Welcome;
