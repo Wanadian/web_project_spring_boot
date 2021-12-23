de08cdbbbd6f0cabcbf626ef2fcbdee1afb76050
Wonkru <williamdpe@gmail.com>
2021-12-23 10:40:00 +0100

    wrong manip

--- app/api/src/main/java/com/server/projet/resources/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.server.projet.resources.exception;
-
-public class BadRequestException extends Exception {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
