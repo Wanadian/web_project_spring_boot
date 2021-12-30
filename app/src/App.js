7884accbad0bf0bc37ddd411b1da668c4c8a4534
Wonkru <williamdpe@gmail.com>
2021-12-30 14:10:50 +0100

    Artist

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Welcome from "./components/Welcome";
-import SongList from "./components/song/SongList";
-import SongDetails from "./components/song/SongDetails";
+import SongList from "./components/song/song/SongList";
+import SongDetails from "./components/song/song/SongDetails";
+import ArtistList from "./components/song/artist/ArtistList";
 
 function App() {
     return (
@@ -12,11 +13,11 @@ function App() {
                     <Route exact path={''} element={<Welcome/>}/>
                     <Route exact path={'/songs'} element={<SongList/>}/>
                     <Route exact path={'/songs/:songId'} element={<SongDetails/>}/>
+                    <Route exact path={'/artists'} element={<ArtistList/>}/>
                 </Routes>
             </Router>
         </div>
-    )
-        ;
+    );
 }
 
 export default App;
---
+++ app/src/components/song/song/SongList.js
@@ -0,0 +1,30 @@
+import Song from "./Song";
+import './SongList.css'
+
+function SongList() {
+    const songs = [
+        {
+            "id": 51,
+            "title": "7 rings",
+            "date": "2019-04-23T18:25:43.511+00:00",
+            "type": "pop",
+            "url": "https://www.youtube.com/watch?v=QYh6mYIJG2Y"
+        },
+        {
+            "id": 52,
+            "title": "Enemy",
+            "date": "2019-04-23T18:25:43.511+00:00",
+            "type": "pop",
+            "url": "https://www.youtube.com/watch?v=F5tSoaJ93ac"
+        }
+    ]
+    return (
+        <div className={"SongList"}>
+            {songs.map((song) =>
+                <Song key={song.id} song={song}/>
+            )}
+        </div>
+    )
+}
+
+export default SongList;
